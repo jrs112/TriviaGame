7b907e4b6391da5f1ac93e73589531043da9315e
john sander <john sander>
2017-03-23 11:10:28 -0400

    final submit one

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ var correct = 0;
 var incorrect = 0;
 var correctAnswer;
 var correctImage;
+var correctAdj;
 var questionOne = false;
 var questionTwo = false;
 var questionThree = false;
@@ -43,8 +44,9 @@ function firstQuestion() {
                         '<p><button type="button" class="btn btn-primary optionButton gerald">Gerald Wallace</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton dell">Dell Curry</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton muggsy">Muggsy Bogues</button></p>');
-    correctAnswer = "Dell Curry!";
+    correctAnswer = "Dell Curry!  He now is the TV commentator for Charlotte Hornets games on Fox Sports South.";
     correctImage = "curry.jpg";
+    correctAdj = "Nice Job!";
     questionOne = true;
     $(".dell").on("click",correctChoice);
     $(".gerald").on("click",wrongChoice);
@@ -61,8 +63,9 @@ function secondQuestion() {
                         '<p><button type="button" class="btn btn-primary optionButton baron">Baron Davis</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton glen">Glen Rice</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton muggsy">Muggsy Bogues</button></p>');
-    correctAnswer = "Baron Davis!";
+    correctAnswer = "Baron Davis!  He buried an 89-foot shot with 0.7 seconds remaining back in 2001.";
     correctImage = "baron.jpg";
+    correctAdj = "Way to go!";
     questionTwo = true;
     $(".baron").on("click",correctChoice);
     $(".gerald").on("click",wrongChoice);
@@ -79,8 +82,9 @@ function thirdQuestion() {
                         '<p><button type="button" class="btn btn-primary optionButton queen">Queen Bee</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton sting">Mr. Sting</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton hugo">Hugo</button></p>');
-    correctAnswer = "Hugo!";
+    correctAnswer = "Hugo!  Hugo is a 4-time winner of the NBA Mascot Slam Dunk Championship, and is also a 2-time winner of the NBA Best Mascot Award. ";
     correctImage = "hugo.jpg";
+    correctAdj = "You know your mascots!";
     questionThree = true;
     $(".hugo").on("click",correctChoice);
     $(".queen").on("click",wrongChoice);
@@ -97,8 +101,9 @@ function fourthQuestion() {
                         '<p><button type="button" class="btn btn-primary optionButton hive">The Hive</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton garden">The Garden</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton mj">The House MJ Built</button></p>');
-    correctAnswer = "The Hive!";
+    correctAnswer = "The Hive!  It is formally known as the Spectrum Center.";
     correctImage = "hive.jpg";
+    correctAdj = "That's right!";
     questionFour = true;
     $(".hive").on("click",correctChoice);
     $(".palace").on("click",wrongChoice);
@@ -115,8 +120,9 @@ function fifthQuestion() {
                         '<p><button type="button" class="btn btn-primary optionButton chapel">UNC Chapel Hill</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton charlotte">UNC Charlotte</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton kentucky">University of Kentucky</button></p>');
-    correctAnswer = "University of Connecticut!";
+    correctAnswer = "UCONN!  Kemba won the championship with UCONN back in 2011.";
     correctImage = "kembawalker.jpg";
+    correctAdj = "You're a champ!";
     questionFive = true;
     $(".uconn").on("click",correctChoice);
     $(".chapel").on("click",wrongChoice);
@@ -133,8 +139,9 @@ function sixthQuestion() {
                         '<p><button type="button" class="btn btn-primary optionButton ninety">90 Points</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton oneHundred">100 Points</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton ninetyFive">95 Points</button></p>');
-    correctAnswer = "95 Points!";
+    correctAnswer = "95 Points!  The promo code is Hornets95.";
     correctImage = "pizzaone.jpg";
+    correctAdj = "You must like pizza!";
     questionSix = true;
     $(".ninetyFive").on("click",correctChoice);
     $(".oneHundredFive").on("click",wrongChoice);
@@ -151,8 +158,9 @@ function seventhQuestion() {
                         '<p><button type="button" class="btn btn-primary optionButton michael">Michael Jordan</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton george">George Shinn</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton larry">Larry Brown</button></p>');
-    correctAnswer = "Michael Jordan!";
+    correctAnswer = "Michael Jordan!  Michael Jordan became the majority owner fo the Charlotte Hornets in 2010 when the Hornets Franchise was known as the Charlotte Bobcats.";
     correctImage = "jordan.jpg";
+    correctAdj = "Space Jam Time!";
     questionSeven = true;
     $(".michael").on("click",correctChoice);
     $(".nellie").on("click",wrongChoice);
@@ -164,13 +172,14 @@ function seventhQuestion() {
 function eigthQuestion() {
     run();
     setTimeout(function(){
-    $(".content").html("<h2>What is the name of the Charlotte Hornets Cheerleaders?</h2>" +
+    $(".content").html("<h2>What is the name of the Charlotte Hornets Dance Team?</h2>" +
                         '<p><button type="button" class="btn btn-primary optionButton queenBees">The Queen Bees</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton theSting">The Sting</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton lady">The Lady Hornets</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton honey">The Honey Bees</button></p>');
-    correctAnswer = "The Honey Bees!";
+    correctAnswer = "The Honey Bees!  There are 21 Honey Bees on the current team.";
     correctImage = "honey.jpg";
+    correctAdj = "Time to dance!";
     questionEight = true;
     $(".honey").on("click",correctChoice);
     $(".queenBees").on("click",wrongChoice);
@@ -187,8 +196,9 @@ function ninthQuestion() {
                         '<p><button type="button" class="btn btn-primary optionButton canada">Canadian</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton france">French</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton algeria">Algerian</button></p>');
-    correctAnswer = "French!";
+    correctAnswer = "French!  Nic attempts to teach his teammates French in hilarious videos on the Jumbotron during Hornets Games.";
     correctImage = "french.jpg";
+    correctAdj = "Parlez-vous francais?";
     questionNine = true;
     $(".france").on("click",correctChoice);
     $(".usa").on("click",wrongChoice);
@@ -205,8 +215,9 @@ function tenthQuestion() {
                         '<p><button type="button" class="btn btn-primary optionButton al">Al Jefferson</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton anthony">Anthony Mason</button></p>' +
                         '<p><button type="button" class="btn btn-primary optionButton alonzo">Alonzo Morning</button></p>');
-    correctAnswer = "Larry Johnson!";
+    correctAnswer = "Larry Johnson!  Emeka Okafor is the franchise leader but that was with the Bobcats before the franchise changed back to the Hornets.";
     correctImage = "larry.jpg";
+    correctAdj = "Great Answer!";
     questionTen = true;
     $(".lj").on("click",correctChoice);
     $(".al").on("click",wrongChoice);
@@ -226,7 +237,7 @@ function outOfTime() {
 }
 
 function correctChoice() {
-    $(".headers").html("<h2>You are correct! The correct answer is " + correctAnswer + "</h2>");
+    $(".headers").html("<h2><strong>" + correctAdj + "</strong> The correct answer is " + correctAnswer + "</h2>");
     clearInterval(clock);
     $(".content").html('<img class="images" src="assets/images/' + correctImage + '"alt="MJ Image">')
     correct++;
@@ -234,7 +245,7 @@ function correctChoice() {
     check();
 }
 function wrongChoice() {
-    $(".headers").html("<h2>You are incorrect! The correct answer is " + correctAnswer + "</h2>");
+    $(".headers").html("<h2><strong>You are incorrect!</strong> The correct answer is " + correctAnswer + "</h2>");
     clearInterval(clock);
     $(".content").html('<img class="images" src="assets/images/marvin.jpeg" alt="Marvin Williams">');
     incorrect++;
@@ -270,15 +281,40 @@ function check() {
         else if(questionTen === false) {
             tenthQuestion();
         }
+        else if(correct > 6 && correct <10) {
+            resultsgreatJob();
+        }
+        else if(correct === 10) {
+            resultsPerfectScore();
+        }
         else {
             results();
-    }   }, 1000 * 3);
+    }   }, 1000 * 6);
 }
 function results() {
-    $(".headers").html("<h1>Your Results!</h1>" +
+    $(".headers").html("<h1>You missed a lot of questions.  Better try again! Here are your results:</h1>" +
                        "<h2>Correct: " + correct + "</h2>" +
                        "<h2>Incorrect: " + incorrect + "</h2>" +
-                       '<p><button type="button" class="btn btn-danger optionButton startOver">Restart</button></p>');
+                       '<p><button type="button" class="btn btn-danger optionButton startOver">Try Again</button></p>');
+    $(".startOver").on("click",restart);
+    clearInterval(clock);
+    $(".content").html('<img class="images" src="assets/images/goggles.jpg" alt="Marvin Goggles">')
+}
+function resultsgreatJob() {
+    $(".headers").html("<h1>Nice Job!  You got most of the questions right!  Try again to see if you can get all of them!  Here are your results:</h1>" +
+                       "<h2>Correct: " + correct + "</h2>" +
+                       "<h2>Incorrect: " + incorrect + "</h2>" +
+                       '<p><button type="button" class="btn btn-danger optionButton startOver">Try Again</button></p>');
+    $(".startOver").on("click",restart);
+    clearInterval(clock);
+    $(".content").html('<img class="images" src="assets/images/goggles.jpg" alt="Marvin Goggles">')
+}
+
+function resultsPerfectScore() {
+    $(".headers").html("<h1>Wow a perfect score! You are a certified Hornets Master!  Here are your results:</h1>" +
+                       "<h2>Correct: " + correct + "</h2>" +
+                       "<h2>Incorrect: " + incorrect + "</h2>" +
+                       '<p><button type="button" class="btn btn-danger optionButton startOver">Try Again</button></p>');
     $(".startOver").on("click",restart);
     clearInterval(clock);
     $(".content").html('<img class="images" src="assets/images/goggles.jpg" alt="Marvin Goggles">')
